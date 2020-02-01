60a7451e73390da18c5e460f9f36b0d0d4c17c66
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2020-02-01 13:04:20 +0100

    add files

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -5,31 +5,7 @@ public static class ResistorColor
     public static int ColorCode(string color)
     {
         color = color.ToLower();
-        switch (color)
-        {
-            case "black":
-                return 0;
-            case "brown":
-                return 1;
-            case "red":
-                return 2;
-            case "orange":
-                return 3;
-            case "yellow":
-                return 4;
-            case "green":
-                return 5;
-            case "blue":
-                return 6;
-            case "violet":
-                return 7;
-            case "grey":
-                return 8;
-            case "white":
-                return 9;
-            default:
-                throw new Exception(color + " is an incorrect color.");
-        }
+        return Array.IndexOf(Colors(), color);
     }
 
     public static string[] Colors()
