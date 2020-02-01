cbdee440af2ef338773ef9b693a97db1c875fd06
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2020-02-01 12:43:04 +0100

    solve exercise

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -4,11 +4,49 @@ public static class ResistorColor
 {
     public static int ColorCode(string color)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        color = color.ToLower();
+        switch (color)
+        {
+            case "black":
+                return 0;
+            case "brown":
+                return 1;
+            case "red":
+                return 2;
+            case "orange":
+                return 3;
+            case "yellow":
+                return 4;
+            case "green":
+                return 5;
+            case "blue":
+                return 6;
+            case "violet":
+                return 7;
+            case "grey":
+                return 8;
+            case "white":
+                return 9;
+            default:
+                throw new Exception(color + " is an incorrect color.");
+        }
     }
 
     public static string[] Colors()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string[] colors =
+        {
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+        };
+        return colors;
     }
 }
